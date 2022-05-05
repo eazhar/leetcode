546825d57ca43c3e5436b86c97456c1d5db7d059
eazhar <eazhar@gmail.com>
2022-05-05 11:55:41 -0700

    Time: 100 ms (5.05%), Space: 54.9 MB (34.47%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,18 +1,37 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        Queue q = new LinkedList();
+        
+        int firstzero = 0;
+        boolean zeroseen = false;
         int oldLength = nums.length;
         for(int i = 0; i<oldLength; i++){
-            if(nums[i]!=0){
-                q.add(nums[i]);
+            if(nums[i]==0 && !zeroseen){
+                firstzero = i;
+                zeroseen=true;
+            }
+            if(nums[i]!=0 && zeroseen){
+                nums[firstzero]=nums[i];
+                nums[i] = 0;
+                i = firstzero;
+                zeroseen=false;
             }
         }
-        int nonzero = q.size();
-        for(int i=0; i<nonzero; i++){
-            nums[i] = (int) q.remove();
-        }
-        for(int i = nonzero; i<oldLength; i++){
-            nums[i] = 0;
-        }
+        
+        
+//          Old solution: Time: 5 ms (14.50%), Space: 55 MB (26.94%) - LeetHub        
+//         Queue q = new LinkedList();
+//         int oldLength = nums.length;
+//         for(int i = 0; i<oldLength; i++){
+//             if(nums[i]!=0){
+//                 q.add(nums[i]);
+//             }
+//         }
+//         int nonzero = q.size();
+//         for(int i=0; i<nonzero; i++){
+//             nums[i] = (int) q.remove();
+//         }
+//         for(int i = nonzero; i<oldLength; i++){
+//             nums[i] = 0;
+//         }
     }
 }
