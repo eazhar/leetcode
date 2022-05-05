da46e9945101715f6897681c73dd15e6c712e9de
eazhar <eazhar@gmail.com>
2022-05-05 12:00:46 -0700

    Time: 7 ms (13.54%), Space: 54.8 MB (41.90%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,37 +1,39 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int firstzero = 0;
-        boolean zeroseen = false;
-        int oldLength = nums.length;
-        for(int i = 0; i<oldLength; i++){
-            if(nums[i]==0 && !zeroseen){
-                firstzero = i;
-                zeroseen=true;
-            }
-            if(nums[i]!=0 && zeroseen){
-                nums[firstzero]=nums[i];
-                nums[i] = 0;
-                i = firstzero;
-                zeroseen=false;
-            }
-        }
+//          Old solution: Time: 100 ms (5.05%), Space: 54.9 MB (34.47%) - LeetHub
+        
+//         int firstzero = 0;
+//         boolean zeroseen = false;
+//         int oldLength = nums.length;
+//         for(int i = 0; i<oldLength; i++){
+//             if(nums[i]==0 && !zeroseen){
+//                 firstzero = i;
+//                 zeroseen=true;
+//             }
+//             if(nums[i]!=0 && zeroseen){
+//                 nums[firstzero]=nums[i];
+//                 nums[i] = 0;
+//                 i = firstzero;
+//                 zeroseen=false;
+//             }
+//         }
         
         
 //          Old solution: Time: 5 ms (14.50%), Space: 55 MB (26.94%) - LeetHub        
-//         Queue q = new LinkedList();
-//         int oldLength = nums.length;
-//         for(int i = 0; i<oldLength; i++){
-//             if(nums[i]!=0){
-//                 q.add(nums[i]);
-//             }
-//         }
-//         int nonzero = q.size();
-//         for(int i=0; i<nonzero; i++){
-//             nums[i] = (int) q.remove();
-//         }
-//         for(int i = nonzero; i<oldLength; i++){
-//             nums[i] = 0;
-//         }
+        Queue q = new LinkedList();
+        int oldLength = nums.length;
+        for(int i = 0; i<oldLength; i++){
+            if(nums[i]!=0){
+                q.add(nums[i]);
+            }
+        }
+        int nonzero = q.size();
+        for(int i=0; i<nonzero; i++){
+            nums[i] = (int) q.remove();
+        }
+        for(int i = nonzero; i<oldLength; i++){
+            nums[i] = 0;
+        }
     }
 }
