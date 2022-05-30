d4a798635b00b05eea6c42a7bd67782e2fe9a1b2
eazhar <eazhar@gmail.com>
2022-05-29 22:02:01 -0700

    Time: 2 ms (87.45%), Space: 68.5 MB (26.97%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,23 +1,36 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        boolean [] dp = new boolean[nums.length];
-        //System.out.println(dp[1]);
-        dp[0] = true;
         
+        int maxIndex = 0;
         for(int i = 0; i<nums.length; i++){
-            if(dp[i]==false){
+            if(i>maxIndex){
                 return false;
             }
-            else if(nums[i]+i>= nums.length-1){
-                return true;
-            }
             else{
-                for(int j = 1; j<nums[i]+1; j++){
-                    dp[i+j] = true;
-                }
+                maxIndex = Math.max(maxIndex, i+nums[i]);
             }
         }
+        return maxIndex >= nums.length-1;
         
-        return dp[dp.length-1];
+//O(n^2) solution
+//         boolean [] dp = new boolean[nums.length];
+//         //System.out.println(dp[1]);
+//         dp[0] = true;
+        
+//         for(int i = 0; i<nums.length; i++){
+//             if(dp[i]==false){
+//                 return false;
+//             }
+//             else if(nums[i]+i>= nums.length-1){
+//                 return true;
+//             }
+//             else{
+//                 for(int j = 1; j<nums[i]+1; j++){
+//                     dp[i+j] = true;
+//                 }
+//             }
+//         }
+         
+//         return dp[dp.length-1];
     }
 }
