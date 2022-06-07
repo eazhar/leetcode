f3d116c5622ff163de0e45bf4ea68e6d4dd2e093
eazhar <eazhar@gmail.com>
2022-06-07 15:37:10 -0700

    Time: 13 ms (98.39%), Space: 41.9 MB (88.99%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,24 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int [] numWays = new int[amount+1];
-        Arrays.fill(numWays, amount+1);
+        Arrays.fill(numWays, 99999);
         numWays[0] = 0;
         
         for(int i = 1; i<(amount+1); i++){
-           // int nWaysCum = i;
             for(int j : coins){
-                //System.out.println(i-j);
                 if((i-j) >=0){
                     numWays[i] = Math.min(numWays[i], 1+numWays[i-j]);
                 }
-                //numWays[i] = nWaysCum;
             }
-            //System.out.println(nWaysCum);
-            // numWays[i] = nWaysCum;
         }
         
-        if(numWays[amount] != amount+1){
-            
+        if(numWays[amount] != 99999){
             return numWays[amount];
         }
         else{
