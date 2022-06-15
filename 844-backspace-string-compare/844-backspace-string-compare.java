3b50f777f4eb59192876aca244093530f97c8a24
eazhar <eazhar@gmail.com>
2022-06-14 21:52:13 -0700

    Time: 15 ms (5.75%), Space: 43 MB (7.74%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,40 +1,107 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack tempS = new Stack();
-        Stack tempT = new Stack();
+        //Optimized Solution
+        int p1 = s.length()-1;
+        int p2 = t.length()-1;
+        int backCount = 0;
         
-        for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i) == '#'){    
-                if(!tempS.isEmpty()){
-                    System.out.println(tempS.pop());
+        while(p1>=0 || p2>=0){
+            while(p1 >= 0 && (backCount > 0 || s.charAt(p1) == '#')){
+                if(s.charAt(p1) == '#'){
+                    backCount++;
                 }
+                else{
+                    backCount--;
+                }
+                    
+               p1--;
             }
-            else{
-                    tempS.push(s.charAt(i));
+            backCount = 0;
+            while(p2 >= 0 && (backCount > 0 || t.charAt(p2) == '#')){
+                if(t.charAt(p2) == '#'){
+                    backCount++;
                 }
-        }
-        
-        for(int i = 0; i<t.length(); i++){
-            if(t.charAt(i) == '#'){
-                if(!tempT.isEmpty()){
-                    System.out.println(tempT.pop());
+                else{
+                    backCount--;
                 }
+                    
+                p2--;
             }
-            else{
-                    tempT.push(t.charAt(i));
+            
+            System.out.println("p1: "+ p1);// + "s.p1: " + s.charAt(p1));
+            System.out.println("p2: "+ p2);// + "t.p2: " + t.charAt(p2));
+                
+            if(p1 >= 0 && p2 >= 0){
+                if(s.charAt(p1) != t.charAt(p2)){
+                    return false;
                 }
-        }
-        
-        if(tempT.size() != tempS.size()){
-            return false;
-        }else{
-            while(!tempT.isEmpty()){                
-                if(!tempT.pop().equals(tempS.pop())){
+                else{
+                   p1--; 
+                   p2--;
+                }
+
+            }else {
+                if(p1 >= 0 || p2 >= 0){
                     return false;
                 }
             }
         }
-        
         return true;
     }
+        
 }
+            
+        
+        
+        //BRUTE FORCE SOLUTION
+//         Stack tempS = new Stack();
+//         Stack tempT = new Stack();
+        
+//         for(int i = 0; i<s.length(); i++){
+//             if(s.charAt(i) == '#'){    
+//                 if(!tempS.isEmpty()){
+//                     System.out.println(tempS.pop());
+//                 }
+//             }
+//             else{
+//                     tempS.push(s.charAt(i));
+//                 }
+//         }
+        
+//         for(int i = 0; i<t.length(); i++){
+//             if(t.charAt(i) == '#'){
+//                 if(!tempT.isEmpty()){
+//                     System.out.println(tempT.pop());
+//                 }
+//             }
+//             else{
+//                     tempT.push(t.charAt(i));
+//                 }
+//         }
+        
+//         if(tempT.size() != tempS.size()){
+//             return false;
+//         }else{
+//             while(!tempT.isEmpty()){                
+//                 if(!tempT.pop().equals(tempS.pop())){
+//                     return false;
+//                 }
+//             }
+//         }
+        
+
+//UNUSED
+
+
+
+
+//         int buffer = 0;
+        
+//         while(s.charAt(p1) == '#'){
+//             p1-=2;
+//         }
+//         while(t.charAt(p2) == '#'){
+//             p2-=2;
+//         }
+        
+//         // System.out.println("Hello");
