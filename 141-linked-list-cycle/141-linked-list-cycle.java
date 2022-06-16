684fcf0779447bdfa0dd13cad354a6e3fdbee20b
eazhar <eazhar@gmail.com>
2022-06-16 14:03:25 -0700

    Time: 1 ms (39.49%), Space: 46.3 MB (28.67%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,17 +11,47 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet <ListNode> h = new HashSet<>();
-        ListNode currentNode = head;
-        while(currentNode!=null){
-            if(h.contains(currentNode)){
+        ListNode p1 = head;
+        ListNode p2 = null;
+        
+        if(p1 == null){
+            return false;
+        }
+        
+        if(p1.next != null){
+            p2 = p1.next;
+        }
+        while(p2!=null){
+            if(p1 == p2){
                 return true;
             }
-            else{
-                h.add(currentNode);
+            p1 = p1.next;
+            if(p2.next != null){
+                if(p2.next.next != null){
+                   p2 = p2.next.next; 
+                }
+                else{
+                    return false;
+                }
+            }else{
+                    return false;
             }
-            currentNode = currentNode.next;
+            
         }
         return false;
     }
 }
+
+//ORIGINAL SOLN
+ // HashSet <ListNode> h = new HashSet<>();
+ //        ListNode currentNode = head;
+ //        while(currentNode!=null){
+ //            if(h.contains(currentNode)){
+ //                return true;
+ //            }
+ //            else{
+ //                h.add(currentNode);
+ //            }
+ //            currentNode = currentNode.next;
+ //        }
+ //        return false;
